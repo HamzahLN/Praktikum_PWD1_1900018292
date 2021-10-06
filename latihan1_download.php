5eb63086c9f0e405657fe93dc06cd1f97033fa1c
HamzahLN <92037983+HamzahLN@users.noreply.github.com>
2021-10-06 21:13:52 +0700

    Add files via upload

---
+++ latihan1_download.php
@@ -0,0 +1,9 @@
+<?php
+$myDir = "D:/File/xampp/htdocs/.vscode/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
